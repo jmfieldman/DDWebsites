4306e36fb2386f68d92ae3ff86fe8fc0998c9a3e
Jason Fieldman <jason@fieldman.org>
2015-03-11 08:09:14 -0700

    First commit

---
+++ Period5/application/controllers/welcome.php
@@ -0,0 +1,49 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		echo 'Hello:<br/>';
+
+		$query = $this->db->query('SELECT * FROM users');
+
+		foreach ($query->result_array() as $row) {
+			echo print_r($row) . '<br/>';
+		}
+
+		//$this->another();
+		//echo 'Foobar';
+		//$this->load->view('welcome_message');
+	}
+
+	public function another($arg) {
+		
+		echo 'Hello:<br/>';
+
+		$query = $this->db->query('SELECT * FROM threads WHERE user_id = ?', array($arg));
+
+		foreach ($query->result_array() as $row) {
+			echo print_r($row) . '<br/>';
+		}
+
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
